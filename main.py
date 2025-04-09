68b9ad26c5860e6b8515d02bba9ac79e86cadaf3
Kostya Vasulyak <vasulyak90@gmail.com>
2025-04-09 16:18:07 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from bot_functions import add_contact, change_contact, show_phone, show_all, add_birthday, show_birthday, birthdays, add_email, show_email, change_email
+from bot_functions import add_contact, edit_phone, show_phone, show_all, add_birthday, show_birthday, birthdays, all_birthdays, add_email, show_email, change_email, find_record, remove_record
 from parse import parse_input
 from address_book import AddressBook  #додали імпорт класу AddressBook
 from validation_functions.validation import name_validation, phone_validation, input_error
@@ -36,11 +36,21 @@ def main():
                 print("Error: Please provide both name and phone number.")
             else:
                 print(add_contact(args, book))  #передаємо book замість contacts
-        elif command == "change":
+        elif command == "change-phone":
             if len(args) < 2:
                 print("Error: Please provide both name and phone number.")
             else:
-                print(change_contact(args, book))  #передаємо book замість contacts
+                print(edit_phone(args, book))  #передаємо book замість contacts
+        elif command == "show-contact":
+            if not args:
+                print("Error: Please provide a name to find the contact.")
+            else:
+                print(find_record(args[0], book))
+        elif command == "remove":
+            if not args:
+                print("Error: Please provide a name to remove the contact.")
+            else:
+                print(remove_record(args[0], book))
         elif command == "phone":
             if not args:
                 print("Error: Please provide a name to find the phone number.")
@@ -60,6 +70,21 @@ def main():
                 print(show_birthday(args[0], book)) #передаємо book замість contacts
         elif command == "birthdays":
             print(birthdays(book)) #передаємо book замість contacts
+        elif command == "all-birthdays":
+            if len(args) < 1:
+                print("Error: Please provide the number of days.")
+            else:
+                try:
+                    number_of_days = int(args[0])
+                    result = book.birthdays_pack(number_of_days)  # book — це AddressBook
+                    if not result:
+                        print("No birthdays found.")
+                    else:
+                        print(f"In {number_of_days} days the birthdays will be for:")
+                        for name, bday in result.items():
+                            print(f"{name}: {bday}")
+                except ValueError:
+                    print("Error: Please provide a valid number of days.")
         elif command == "add-email":
             if len(args) < 2:
                 print("Error: Please provide both name and email.")
